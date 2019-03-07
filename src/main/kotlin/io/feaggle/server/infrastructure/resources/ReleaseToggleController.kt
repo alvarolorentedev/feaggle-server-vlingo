88dca8f1ef00165f5ce7360d64c0a465fa52b9e6
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-03-07 22:16:29 +0100

    Enable/disable toggle

--- src/main/kotlin/io/feaggle/server/infrastructure/resources/ReleaseToggleController.kt
+++ src/main/kotlin/io/feaggle/server/infrastructure/resources/ReleaseToggleController.kt
@@ -13,12 +13,17 @@ import java.util.*
 
 class ReleaseToggleController(private val world: World, private val journal: Journal<String>, private val poolSize: Int) {
     data class CreateReleaseCommand(val name: String)
+    data class ChangeReleaseStatusCommand(val enabled: Boolean)
 
     fun asResource() = resource(
         "releases", poolSize,
         get("/release/{id}")
             .param(String::class.java)
             .handle(this::getRelease).onError(this::onError),
+        put("/release/{id}")
+            .param(String::class.java)
+            .body(ChangeReleaseStatusCommand::class.java)
+            .handle(this::changeReleaseStatus).onError(this::onError),
         get("/releases")
             .handle(this::allReleases).onError(this::onError),
         post("/release")
@@ -36,6 +41,19 @@ class ReleaseToggleController(private val world: World, private val journal: Jou
                     Response.of(Response.Status.NotFound)
                 }
             }
+
+    private fun changeReleaseStatus(id: String, command: ChangeReleaseStatusCommand): Completes<Response> {
+        return world.releaseOf(UUID.fromString(id))
+            .andThenConsume { release ->
+                when (command.enabled) {
+                    true -> release.enable()
+                    false -> release.disable()
+                }
+            }.andThenTo {
+                it.state()
+            }
+            .andThen { state -> Response.of(Response.Status.Created, serialized(state.get())) }
+    }
 
     private fun allReleases() =
         world.library(journal)

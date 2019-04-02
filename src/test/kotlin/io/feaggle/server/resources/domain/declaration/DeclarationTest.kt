887d6889a79f204ce09983a89933715073294b0c
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-04-02 22:59:28 +0200

    Add license

--- src/test/kotlin/io/feaggle/server/resources/domain/declaration/DeclarationTest.kt
+++ src/test/kotlin/io/feaggle/server/resources/domain/declaration/DeclarationTest.kt
@@ -1,3 +1,19 @@
+/**
+ * This file is part of feaggle-server.
+ *
+ * feaggle-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * feaggle-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with feaggle-server.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 package io.feaggle.server.resources.domain.declaration
 
 import io.feaggle.server.base.UnitTest
--- src/test/kotlin/io/feaggle/server/specs/toggles/ReleaseToggleTest.kt
+++ src/test/kotlin/io/feaggle/server/specs/toggles/ReleaseToggleTest.kt
@@ -1,8 +1,23 @@
+/**
+ * This file is part of feaggle-server.
+ *
+ * feaggle-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * feaggle-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with feaggle-server.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 package io.feaggle.server.specs.toggles
 
 import io.feaggle.server.specs.Specification
 import io.restassured.RestAssured.given
-import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 import java.util.*
 
